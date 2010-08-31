8d508eb1a4e2d677b564a74b8c7151340e85594e
Peter Griess <pg@std.in>
2010-08-30 18:42:43 -0700

    Remove GPLv2 wording in license.
    - This is as per instructions in the license itself if you wish to avoid
      ambiguities about BSD vs. GPL licensing.

--- deps/libcoro/coro.h
+++ deps/libcoro/coro.h
@@ -22,17 +22,6 @@
  * ERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Alternatively, the contents of this file may be used under the terms of
- * the GNU General Public License ("GPL") version 2 or any later version,
- * in which case the provisions of the GPL are applicable instead of
- * the above. If you wish to allow the use of your version of this file
- * only under the terms of the GPL and not to allow others to use your
- * version of this file under the BSD license, indicate your decision
- * by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL. If you do not delete the
- * provisions above, a recipient may use your version of this file under
- * either the BSD or the GPL.
- *
  * This library is modelled strictly after Ralf S. Engelschalls article at
  * http://www.gnu.org/software/pth/rse-pmt.ps. So most of the credit must
  * go to Ralf S. Engelschall <rse@engelschall.com>.
@@ -212,7 +201,7 @@ void coro_destroy (coro_context *ctx);
 # elif defined(HAVE_SETJMP_H) && defined(HAVE_SIGALTSTACK)
 #  define CORO_SJLJ 1
 # else
-error unknown or unsupported architecture
+#error unknown or unsupported architecture
 # endif
 #endif
 
