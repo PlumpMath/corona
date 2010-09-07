f0bf87a7a9540ae6497708f7568e37792065c263
Peter Griess <pg@std.in>
2010-09-07 15:49:27 -0500

    Add -c switch to run coro vs. pure libev.

--- src/benchd.c
+++ src/benchd.c
@@ -11,14 +11,27 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <coro.h>
 #include "common.h"
+
+// XXX: Not sure if MINSIGSTKSZ is very portable
+#define CORO_STACK_SZ                   MINSIGSTKSZ
 
 typedef struct sock_watcher_s {
     struct ev_io sw_ev_io;
     int sw_sock;
 } sock_watcher_t;
 
+typedef struct read_watcher_s {
+    sock_watcher_t rw_sock;
+    struct coro_context rw_coro_ctx;
+    void *rw_coro_stack;
+    int rw_done;
+} read_watcher_t;
+
 static char buf[(1 << 20)];
+static int use_coro = 0;
+struct coro_context el_ctx;
 
 // stats
 size_t watcher_run_cnt = 0;
@@ -37,9 +50,39 @@ static void sigint_cb(int sig) {
     exit(0);
 }
 
+// Do the meat of the read watcher; sets rw->rw_done
+static void do_read_watcher(read_watcher_t *rw) {
+    sock_watcher_t *sw = (sock_watcher_t*) rw;
+    int nbytes;
+
+    do {
+        nbytes = read(sw->sw_sock, buf, sizeof(buf));
+        errno_cnt_incr(&read_errors_cnt, nbytes);
+        DBG(
+            1,
+            ("read %d bytes from socket %d; errno %d\n",
+                nbytes, sw->sw_sock, (nbytes < 0) ? errno : 0)
+        );
+    } while (nbytes > 0);
+
+    rw->rw_done = (nbytes == 0) ||
+                  (nbytes < 0 && errno != EAGAIN);
+}
+
+// Read coro; calls do_read_watcher() with coro juice
+static void read_watcher_coro(read_watcher_t *rw) {
+    while (1) {
+        do_read_watcher(rw);
+        coro_transfer(&rw->rw_coro_ctx, &el_ctx);
+    }
+
+    assert(!"Broke out of read_watcher_coro() event loop!");
+}
+
+// Read callback; dispatches to coro or calls do_read_watcher() itself
 static void read_watcher_cb(struct ev_loop *el, ev_io *ew, int revents) {
     sock_watcher_t *sw = (sock_watcher_t*) ew;
-    int nbytes = 0;
+    read_watcher_t *rw = (read_watcher_t*) ew;
 
     if (!(revents & EV_READ)) {
         return;
@@ -47,19 +90,19 @@ static void read_watcher_cb(struct ev_loop *el, ev_io *ew, int revents) {
 
     watcher_run_cnt++;
 
-    do {
-        nbytes = read(sw->sw_sock, buf, sizeof(buf));
-        errno_cnt_incr(&read_errors_cnt, nbytes);
-        DBG(1, ("read %d bytes from socket %d\n", nbytes, sw->sw_sock));
-    } while (nbytes > 0);
+    if (use_coro) {
+        coro_transfer(&el_ctx, &rw->rw_coro_ctx);
+    } else {
+        do_read_watcher(rw);
+    }
 
-    if (nbytes == 0 ||
-        (nbytes < 0 && errno != EAGAIN)) {
-        DBG(
-            0,
-            ("read %d bytes from socket; errno %d\n",
-                nbytes, (nbytes < 0) ? errno : 0)
-        );
+    if (rw->rw_done) {
+        DBG(0, ("closing socket %d\n", sw->sw_sock));
+
+        if (use_coro) {
+            // coro_destroy(&rw->rw_coro_ctx);
+            free(rw->rw_coro_stack);
+        }
 
         ev_io_stop(el, &sw->sw_ev_io);
         close(sw->sw_sock);
@@ -68,11 +111,11 @@ static void read_watcher_cb(struct ev_loop *el, ev_io *ew, int revents) {
 
 static void accept_watcher_cb(struct ev_loop *el, ev_io *ew, int revents) {
     sock_watcher_t *sw = (sock_watcher_t*) ew;
+    read_watcher_t *rw;
     struct sockaddr_in addr;
     socklen_t addrlen = sizeof(addr);
     int sock;
     int err;
-    sock_watcher_t *sw2;
 
     if (!(revents & EV_READ)) {
         return;
@@ -91,14 +134,25 @@ static void accept_watcher_cb(struct ev_loop *el, ev_io *ew, int revents) {
         return;
     }
 
-    DBG(0, ("acepted socket %d\n", sock));
+    DBG(0, ("accepted socket %d\n", sock));
     
-    sw2 = (sock_watcher_t*) malloc(sizeof(*sw2));
+    rw = (read_watcher_t*) malloc(sizeof(read_watcher_t));
+    sw = (sock_watcher_t*) rw;
 
-    sw2->sw_sock = sock;
-    ev_init(&sw2->sw_ev_io, read_watcher_cb);
-    ev_io_set(&sw2->sw_ev_io, sock, EV_READ);
-    ev_io_start(el, &sw2->sw_ev_io);
+    sw->sw_sock = sock;
+    ev_init(&sw->sw_ev_io, read_watcher_cb);
+    ev_io_set(&sw->sw_ev_io, sock, EV_READ);
+
+    if (use_coro) {
+        rw->rw_coro_stack = malloc(CORO_STACK_SZ);
+        rw->rw_done = 0;
+        coro_create(
+            &rw->rw_coro_ctx, (coro_func) read_watcher_coro, rw, rw->rw_coro_stack,
+            CORO_STACK_SZ
+        );
+    }
+
+    ev_io_start(el, &sw->sw_ev_io);
 }
 
 void usage(FILE *fp, char *name) {
@@ -108,6 +162,8 @@ void usage(FILE *fp, char *name) {
 "Run the TCP benchmarking server, listening on the given hostname and port.\n");
     fprintf(fp,
 "\nOptions:\n");
+    fprintf(fp,
+"  -c                benchmark using coroutines (default: pure libev)\n");
     fprintf(fp,
 "  -h                help\n");
     fprintf(fp,
@@ -121,10 +177,15 @@ int main(int argc, char **argv) {
     sock_watcher_t sw;
     int c;
     struct hostent *hent;
+    void *el_stack;
 
     optreset = optind = 1;
-    while ((c = getopt(argc, argv, "hv")) != -1) {
+    while ((c = getopt(argc, argv, "hvc")) != -1) {
         switch (c) {
+        case 'c':
+            use_coro = 1;
+            break;
+
         case 'h':
             usage(stdout, argv[0]);
             return 0;
@@ -184,6 +245,11 @@ int main(int argc, char **argv) {
 
     el = ev_default_loop(EVFLAG_AUTO);
 
+    if (use_coro) {
+        el_stack = malloc(CORO_STACK_SZ);
+        coro_create(&el_ctx, NULL, NULL, el_stack, CORO_STACK_SZ);
+    }
+
     sw.sw_sock = sock;
     ev_init(&sw.sw_ev_io, accept_watcher_cb);
     ev_io_set(&sw.sw_ev_io, sock, EV_READ);
@@ -191,6 +257,11 @@ int main(int argc, char **argv) {
 
     ev_loop(el, 0);
     ev_default_destroy();
+
+    if (use_coro) {
+        // coro_destroy(&el_ctx);
+        free(el_stack);
+    }
 
     return 0;
 }
